675214bcefb44317db6b7c50813c9e27d045f4b0
Deniz Tokmakoglu <75509096+deniztokmakoglu@users.noreply.github.com>
2022-05-10 11:07:02 -0500

    removed spacy tokenizer

--- data/data_process.py
+++ data/data_process.py
@@ -28,10 +28,6 @@ def read_experiment_file(filename, pandas = False, tokenizer = None):
                     tk_object = WordPunctTokenizer()
                     line[0] = tk_object.tokenize(line[0])
                     line[0] = [word.lower() for word in line[0]]
-                elif tokenizer == "spacy":
-                    nlp = spacy.load("en_core_web_sm", disable=["tagger", "parser", "ner"])
-                    line[0] = nlp(line[0])
-                    line[0] = [word.lower() for word in line[0]]
                 data.append(line)
         f.close()
         return data, len(data)
