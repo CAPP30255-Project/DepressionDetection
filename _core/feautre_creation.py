70ff138e1abe5c9486facc0ca1876d5a43fd57a6
Deniz Tokmakoglu <75509096+deniztokmakoglu@users.noreply.github.com>
2022-05-10 16:38:44 -0500

    optim prob fixed

--- _core/feautre_creation.py
+++ _core/feautre_creation.py
@@ -9,6 +9,7 @@ import torch
 from torch.utils.data import DataLoader
 
 LABEL_MAPPINGS = {"suicide": 1, "non-suicide": 0}
+DEVICE = 'cuda' # change if working on a CPU
 
 def bow_classifier(data):
     counter = Counter()
@@ -18,16 +19,16 @@ def bow_classifier(data):
     vocab = Vocab(counter)
     return vocab, counter
 
-def collate_into_bow(batch):
+def collate_into_bow(batch, device = DEVICE):
     labels = [0] * len(batch)
     vectors = torch.zeros(len(batch), len(vocab))
     for index, (words, label) in enumerate(batch):
         labels[index] = LABEL_MAPPINGS[label]
         for word in words:
-            index_word = vocab[word]
+            index_word = vocab[word]    
             vectors[index, int(index_word)] += 1 / len(words)     
     labels = torch.tensor(labels)
-    return labels, vectors
+    return labels.to(device), vectors.to(device)
 
 def data_loader_bow(data, batch_size, shuffle = False):
     vocab = bow_classifier(data)
