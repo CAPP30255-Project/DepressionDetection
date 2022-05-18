387fec9fb7da9f740d460fbadbb25b14a8481ff1
Deniz Tokmakoglu <75509096+deniztokmakoglu@users.noreply.github.com>
2022-05-18 12:04:52 -0500

    glove

--- _core/feautre_creation.py
+++ _core/feautre_creation.py
@@ -166,6 +166,7 @@ def embed_data(data, glove):
 
 def collate_into_cbow_glove(object, embedding_dim = 300, device = DEVICE):
     batch, glove_embeddings, vocab = object
+    print(vocab)
     labels = [0] * len(batch)
     vocab_size = len(vocab)
     vectors = torch.zeros(len(batch), vocab_size, embedding_dim)
