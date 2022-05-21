8d403c55e958200899132d390368a96cd39e84b4
Deniz Tokmakoglu <75509096+deniztokmakoglu@users.noreply.github.com>
2022-05-21 13:49:59 -0500

    ?

--- _core/bi_LSTM.py
+++ _core/bi_LSTM.py
@@ -63,7 +63,7 @@ class RNNDepressionClassifier(nn.Module):
         # apply dropout to the input
         # Shape of inputs: (batch_size, sequence_length, embedding_dim)
         print(inputs.shape, 'shape of input')
-        embedded_input = self.embedding(inputs)
+        embedded_input = self.embedding(inputs.long())
         print(embedded_input.shape, 'embedding shape')
         embedded_input = self.dropout_on_input_to_LSTM(embedded_input)
         print(embedded_input.shape, 'embedding shape after dropout')
